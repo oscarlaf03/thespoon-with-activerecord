0ed23398d6a6e496a1e0037cc8c682e381e0c7c3
oscar ortiz <ortizg.oscar@gmail.com>
2018-03-27 10:09:14 -0300

    Added column address tor estaurants, addes restaurants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # config/routes.rb
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/:id", to: "restaurants#show"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327122311) do
+ActiveRecord::Schema.define(version: 20180327124030) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "names"
     t.integer "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
