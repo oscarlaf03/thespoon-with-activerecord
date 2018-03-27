dfedd7d814374742af9b46aa37904f2d7409d933
oscar ortiz <ortizg.oscar@gmail.com>
2018-03-27 10:50:40 -0300

    Added  routes via resources commands and created views from the rails generate controller comand naming the propper views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,29 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # config/routes.rb
   ## What do I want to do (CRUD)?
-  # List all restaurants
-  # View a specific restaurant
-  # Create a Restaurant
-  # Update a Restaurant
-  # Delete a Restaurnt
+  # 1 List all restaurants
+  resources :restaurants
 
+=begin
+  get 'restaurants', to: 'restaurants#index'
 
+  # 2 View a specific restaurant
+  get 'restaurant/:id', to: 'restaurants#show'
+
+  # 3 Create a Restaurant
+  get 'restaurants/new', to: 'restaurants#new' #por convenção bota plura na rota do 'new'
+
+  post 'restaurants', to: 'restaurants#create'
+
+  # 4 Update a Restaurant
+  get 'restaurant/:id/edit', to:'restaurants#edit'
+
+  patch 'restaurant/:id', to: 'restaurants#update'
+
+  # 5 Delete a Restaurant
+  delete 'restaurant/id:', to: 'restaurants#destroy'
+=end
 
 end
